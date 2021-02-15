40e14ab73542069fe0e53748e2ba61d9cf56a81f
Steven Smail <41452253+ssmail7@users.noreply.github.com>
2021-02-15 16:31:07 -0500

    Add files via upload

---
+++ Car.java
@@ -0,0 +1,50 @@
+//************************************************
+//  Car.java
+//
+//  Represents a car.
+//
+//************************************************
+
+public class Car
+{
+    // Instance variables
+    private String make;
+    private String model;
+    private int year;	
+
+    //-----------------------------------------------------
+    // Constructor - sets up a car object by initializing 
+    // the make, the model, and the year.
+    //-----------------------------------------------------
+    public Car(String newMake, String newModel, int newYear)
+    {
+        make = newMake;
+        model = newModel;
+        year = newYear;
+    }
+   
+    //--------------------------------------------------------------  
+    // Method isAntique that takes no parameter. The method 
+    // should compute and return true if the car is more than 25
+    // years old, and false if otherwise.
+    //--------------------------------------------------------------
+    public boolean isAntique()
+    {
+        if (2016 - year > 25)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+     
+    //------------------------------------------------------
+    // Returns a string representation of a car.
+    //------------------------------------------------------
+    public String toString()
+    {
+        return year + " " + make + " " + model;
+    }
+}
---
+++ Car_Test.java
@@ -0,0 +1,49 @@
+//**************************************************
+//  Car_Test.java
+//
+//  A test driver for class Car.
+//**************************************************
+import java.util.Scanner;
+
+public class Car_Test
+{
+//-----------------------------------------------
+// Creates a car object and then calls the 
+// isAntique method.
+//-----------------------------------------------
+    public static void main (String[] args)
+    {
+        String carMake;
+        String carModel;
+        int carYear;
+
+        Scanner scan = new Scanner(System.in);
+        System.out.print ("Enter the make of the car: ");
+        carMake = scan.nextLine();
+
+        System.out.print ("Enter the model of the car: ");
+        carModel = scan.nextLine();
+
+        System.out.print ("Enter the year of the car: ");
+        carYear = scan.nextInt();
+
+        // Create a car object 
+        Car car1 = new Car(carMake, carModel, carYear);
+        System.out.println();
+
+        // Print summary for the car 
+        System.out.println(car1);
+        System.out.println();
+
+        if (car1.isAntique())
+        {
+            System.out.println("This car is an antique");
+            System.out.println();
+        }
+        else
+        {
+            System.out.println("This car is not an antique");
+            System.out.println();
+        }
+    }
+}
